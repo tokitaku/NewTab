9908607b16e614f99857cabcf3a737896f14c8e2
tokitaku <tokitaku@users.noreply.github.com>
2026-04-06 00:20:56 +0000

    deploy: 769afd9e8ef5d11c32efd2f430131ecd5cc4fd99

--- _next/static/QbVlwtsTNhev1aFY9TS2G/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a02997e2ea120e16.css","static/chunks/pages/index-5db4880dfdabde22.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
